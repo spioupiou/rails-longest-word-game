942ab7cf3641b25872ec62672ddcb581562a7b2f
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-01-07 12:49:19 +0900

    Functionning app with highest score kept

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,42 +3,71 @@ require 'json'
 
 class GamesController < ApplicationController
   def new
-    alphabet = %w[A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+    consonants = %w[B C D F G H J K L M N P Q R S T V W X Y Z]
+    vowels = %w[A E I O U]
     @letters = []
-    10.times do
-      @letters << alphabet[rand(26)]
+    @score = session[:score]
+
+    6.times do
+      @letters << consonants[rand(21)]
     end
+
+    4.times do
+      @letters << vowels[rand(5)]
+    end
+
+    @letters.shuffle!
   end
 
   def score
     @user_input = params["word"].upcase
-    @letters = params["letters"]
+    @letters = JSON.parse(params["letters"].tr("'", '"'))
 
-    token = params[:authenticity_token]
     api_hash = generate_hash(@user_input)
 
-    if token
-      return @message = "Sorry but #{@user_input} can't be build out of #{@letters}" unless analyze_word(@user_input, @letters)
+    return @message = "Sorry, #{@user_input} can't be build out of #{@letters}" unless analyze_word(@user_input, @letters)
 
-      @message = api_hash['found'] ? "Congrats! #{@user_input} is a valid English word!" : 'Sorry, not an English word'
+    if api_hash['found']
+      @message =  "Congrats! #{@user_input} is a valid English word!"
+      session[:score] = generate_score(@user_input)
     else
-      @message = 'ACCESS DENIED'
+      @message = "Sorry, #{@user_input} is not an English word"
     end
   end
-end
 
-private
-
-def analyze_word(word, array)
-  word = word.split('')
-  word.each do |letter|
-    return false unless array.include?(letter)
+  def reset
+    reset_session
+    redirect_to('/new')
   end
-  true
-end
 
-def generate_hash(keyword)
-  url = "https://wagon-dictionary.herokuapp.com/#{keyword}"
-  html = URI.open(url).read
-  JSON.parse(html)
+  private
+
+  def analyze_word(word, array)
+    word = word.split('')
+    array_cp = array.clone
+
+    word.each do |letter|
+      return false unless array_cp.include?(letter)
+
+      array_cp.delete_at(array_cp.index(letter))
+    end
+
+    true
+  end
+
+  def generate_hash(keyword)
+    url = "https://wagon-dictionary.herokuapp.com/#{keyword}"
+    html = URI.open(url).read
+    JSON.parse(html)
+  end
+
+  def generate_score(word_length)
+    if session[:score].nil?
+      session[:score] = @user_input.length
+    elsif @user_input.length > session[:score]
+      session[:score] = @user_input.length
+    else
+      return session[:score]
+    end
+  end
 end
